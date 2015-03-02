d09a1cef4f194a04afc2ffeaff69794676e7860c
Adam Biesenbach <abiesenb@gmail.com>
2015-03-02 18:58:26 -0500

    Test commit

---
+++ arb348/answer4.py
@@ -0,0 +1,20 @@
+'''
+Created on Mar 2, 2015
+
+@author: ds-ga-1007
+'''
+
+var1 = (3*5)/(2+3)
+print var1
+
+var2 = ((7+9)**(0.5))*2
+print var2
+
+var3 = (4-7)**3
+print var3 
+
+var4 = (-19+100)**(0.25)
+print var4
+
+var5 = 6%4
+print var5
