197f1dbde9b0ea486b3e0a84516587e70ddbd751
Adam Biesenbach <abiesenb@gmail.com>
2015-03-02 23:18:55 -0500

    Commit changes to answer6

--- arb348/answer6.py
+++ arb348/answer6.py
@@ -9,9 +9,9 @@ Created on Mar 2, 2015
 FirstName = raw_input("Enter your first name: ")
 LastName  = raw_input("Enter your last name: ")
 print "Enter your date of birth: "
-Month = raw_input("Month? ")
-Day = raw_input("Day? ")
-Year = raw_input("Year? ")
+Month = raw_input("Month (in string form, e.g. 'January')? ")  
+Day = raw_input("Day (as a number)? ")
+Year = raw_input("Year (as a number)? ")
 
 # Print the formatted results, as done in the HW question. 
 
