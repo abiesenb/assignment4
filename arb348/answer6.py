6326fe3044796f2c20b9704fdc380fbe272bea5a
Adam Biesenbach <abiesenb@gmail.com>
2015-03-02 23:07:19 -0500

    Commit Update to question 6.

--- arb348/answer6.py
+++ arb348/answer6.py
@@ -4,6 +4,8 @@ Created on Mar 2, 2015
 @author: ds-ga-1007
 '''
 
+# Get the input from the user. Here I just did the same process as in the HW question.
+
 FirstName = raw_input("Enter your first name: ")
 LastName  = raw_input("Enter your last name: ")
 print "Enter your date of birth: "
@@ -11,5 +13,6 @@ Month = raw_input("Month? ")
 Day = raw_input("Day? ")
 Year = raw_input("Year? ")
 
+# Print the formatted results, as done in the HW question. 
 
 print  FirstName + " " + LastName + " was born on " + Month + " " + Day + ", " + Year + "."
